face05cf7e4c210468ad4d0190f9c1d8280f0136
Awodire2 <159066434+Awodire2@users.noreply.github.com>
2025-01-29 14:00:51 +0100

    Add files via upload

---
+++ serenitytouchmassageandspa.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("button")
+function button() {
+    if (button = onclick) {
+        document.getElementById('button').style.display = "none" ;
+    }
+    else {
+        document.getElementById('button').style.display = "block" ;
+    }
+}
